88b0c5b97b015e3bedb8b2fe64172aa34c1811d5
vicgui <33523087+vicgui@users.noreply.github.com>
2022-06-08 21:14:20 +0100

    minor improvements

--- server/src/resources/team/team.controllers.js
+++ server/src/resources/team/team.controllers.js
@@ -1,13 +1,27 @@
-const Team = require('./team.model')
+const Team = require("./team.model");
 
-const findMany = async(req,res) =>{
-    try{
-        const docs = await Team.find().lean().exec();
-        res.status(200).json({results:docs})
-    }catch(e){
-        console.log(e);
-        res.status(500).json({ error: 'Internal error' });
+const findMany = async (req, res) => {
+  try {
+    const docs = await Team.find().lean().exec();
+    res.status(200).json({ results: docs });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ error: "Internal error" });
+  }
+};
+
+const findOne = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const doc = await Team.findOne({ _id: id });
+    if (!doc) {
+      return res.status(404).json({ error: "Not found" });
     }
-}
+    res.status(200).json({ results: [doc] });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ error: "Internal error" });
+  }
+};
 
-module.exports = {findMany}
+module.exports = { findMany, findOne };
--- server/src/resources/team/team.router.js
+++ server/src/resources/team/team.router.js
@@ -1,11 +1,12 @@
-const { Router } = require("express")
+const { Router } = require("express");
 const { route } = require("express/lib/application");
 
-const teamControllers = require('./team.controllers');
+const teamControllers = require("./team.controllers");
 
 const router = Router();
 
-router.route("/")
-    .get(teamControllers.findMany)
+router.route("/").get(teamControllers.findMany);
+
+router.route("/:id").get(teamControllers.findOne);
 
 module.exports = router;
